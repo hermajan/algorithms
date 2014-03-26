cbf3fe367ed4128b4cac755acd9f1993df23cddc
DJohnny <hermajan@gmail.com>
2014-03-26 22:32:31 +0100

    Added main class.

---
+++ src/algorithms/Main.java
@@ -0,0 +1,14 @@
+package algorithms;
+
+/**
+ * Main class.
+ * @author DJohnny
+ */
+public class Main {
+    /**
+     * @param args The command line arguments.
+     */
+    public static void main(String[] args) {
+
+    }    
+}
