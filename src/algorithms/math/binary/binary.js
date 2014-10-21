4ae2eb48048da890b78df4d001adeeab78f25398
DJohnny <hermajan@gmail.com>
2014-10-21 23:30:34 +0200

    Added conversion integer to binary in JavaScript.

---
+++ src/algorithms/math/binary/binary.js
@@ -0,0 +1,8 @@
+/**
+ * Converts decimal number to binary using string.
+ * @param {type} number Number to convert.
+ * @return {String} String with binary number.
+ */
+function dec2bin(number) {
+  return new Number(number).toString(2);
+}
