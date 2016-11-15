855649074a1aed139bc8ea185f607544a0cb7fba
Jan Hermann <hermajan@gmail.com>
2016-11-15 19:01:32 +0100

    Fixed Factorial.

--- src/algorithms/math/Factorial.java
+++ src/algorithms/math/Factorial.java
@@ -2,30 +2,43 @@ package algorithms.math;
 
 /**
  * Implementations of the factorial.
- * @author DJohnny
  */
 public class Factorial {
-    /**
-     * Iterative algorithm for calculating factorial.
-     * @param number Non-negative integer.
-     * @return Factorial of the number.
-     */
-    public static long factorialIterative(long number) {
-        if(number==0) { return 1; }
-        long output=number;
-        for(long i=1;i<number;i++) {
-            output=output*(number-i);
-        }
-        return output;
-    }
-    
-    /**
-     * Recursive algorithm for calculating factorial.
-     * @param number Non-negative integer.
-     * @return Factorial of the number.
-     */
-    public static long factorialRecursive(long number) {
-        if(number==0) { return 1; }
-        return number*factorialRecursive(number-1);
-    }
+	/**
+	 * Iterative algorithm for calculating factorial.
+	 * @param number Non-negative integer.
+	 * @return Factorial of the number.
+	 */
+	public static long factorialIterative(long number) {
+		if(number == 0) { return 1; }
+		long output = 1;
+		for(long i = number; i > 1; i--) {
+			output *= i;
+		}
+		return output;
+	}
+
+	/**
+	 * Iterative algorithm for calculating factorial forwardly.
+	 * @param number Non-negative integer.
+	 * @return Factorial of the number.
+	 */
+	public static long factorialIterativeForward(long number) {
+		if(number == 0) { return 1; }
+		long output = number;
+		for(long i=1; i<number; i++) {
+			output *= number-i;
+		}
+		return output;
+	}
+	
+	/**
+	 * Recursive algorithm for calculating factorial.
+	 * @param number Non-negative integer.
+	 * @return Factorial of the number.
+	 */
+	public static long factorialRecursive(long number) {
+		if(number==0) { return 1; }
+		return number*factorialRecursive(number-1);
+	}
 }
--- test/algorithms/math/FactorialTest.java
+++ test/algorithms/math/FactorialTest.java
@@ -5,26 +5,35 @@ import org.junit.Test;
 
 /**
  * Tests for the Factorial class.
- * @author DJohnny
  */
 public class FactorialTest {
-    /**
-     * Test of factorialIterative method, of class Factorial.
-     */
-    @Test
-    public void testFactorialIterative() {
-        System.out.println("factorialIterative");
-        assertEquals(1, Factorial.factorialIterative(0), 0);
-        assertEquals(120, Factorial.factorialIterative(5), 0);
-    }
+	/**
+	 * Test of factorialIterative method, of class Factorial.
+	 */
+	@Test
+	public void testFactorialIterative() {
+		System.out.println("factorialIterative");
+		assertEquals(1, Factorial.factorialIterative(0), 0);
+		assertEquals(120, Factorial.factorialIterative(5), 0);
+	}
 
-    /**
-     * Test of factorialRecursive method, of class Factorial.
-     */
-    @Test
-    public void testFactorialRecursive() {
-        System.out.println("factorialRecursive");
-        assertEquals(1, Factorial.factorialRecursive(0), 0);
-        assertEquals(120, Factorial.factorialRecursive(5), 0);
-    }
+	/**
+	 * Test of factorialIterativeForward method, of class Factorial.
+	 */
+	@Test
+	public void testFactorialIterativeForward() {
+		System.out.println("factorialIterativeForward");
+		assertEquals(1, Factorial.factorialIterativeForward(0), 0);
+		assertEquals(120, Factorial.factorialIterativeForward(5), 0);
+	}
+
+	/**
+	 * Test of factorialRecursive method, of class Factorial.
+	 */
+	@Test
+	public void testFactorialRecursive() {
+		System.out.println("factorialRecursive");
+		assertEquals(1, Factorial.factorialRecursive(0), 0);
+		assertEquals(120, Factorial.factorialRecursive(5), 0);
+	}
 }
